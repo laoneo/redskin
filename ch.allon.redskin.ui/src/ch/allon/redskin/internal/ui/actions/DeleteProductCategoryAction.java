0e09a1c49322930dd782c152756b8584953ed386
allon.moritz <allon.moritz@b4a91378-5cb6-11de-b31f-81d54249e7d9>
2009-12-04 16:27:25 +0000

    delete is working properly!!!!

--- ch.allon.redskin.ui/src/ch/allon/redskin/internal/ui/actions/DeleteProductCategoryAction.java
+++ ch.allon.redskin.ui/src/ch/allon/redskin/internal/ui/actions/DeleteProductCategoryAction.java
@@ -2,26 +2,42 @@ package ch.allon.redskin.internal.ui.actions;
 
 import java.util.List;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.emf.ecore.EObject;
 
 import ch.allon.redskin.core.DBFactory;
+import ch.allon.redskin.core.IJobRunnable;
 import ch.allon.redskin.core.model.shop.ProductCategory;
+import ch.allon.redskin.internal.ui.UIUtil;
 
 public class DeleteProductCategoryAction extends EObjectAction {
 
 	@Override
-	protected void run(List<EObject> selectedObjects) {
+	protected void run(final List<EObject> selectedObjects) {
 		if (selectedObjects.size() < 1)
 			return;
-		for (EObject obj : selectedObjects) {
-			if (obj instanceof ProductCategory) {
-				ProductCategory p = (ProductCategory) obj;
-				if (p.getParent() == null)
-					DBFactory.getProductsResource().getContents().remove(p);
-				else
-					p.getParent().getSubCategorys().remove(p);
+		UIUtil.runUIJob(new IJobRunnable() {
+
+			@Override
+			public IStatus run(IProgressMonitor monitor) {
+				for (EObject obj : selectedObjects) {
+					if (obj instanceof ProductCategory) {
+						ProductCategory p = (ProductCategory) obj;
+						p.getProducts().clear();
+						p.getSubCategorys().clear();
+						if (p.getParent() == null)
+							DBFactory.getProductsResource().getContents()
+									.remove(p);
+						else {
+							p.getParent().getSubCategorys().remove(p);
+						}
+					}
+				}
+				return Status.OK_STATUS;
 			}
-		}
+		});
 	}
 
 }
--- ch.allon.redskin.ui/src/ch/allon/redskin/internal/ui/views/ProductView.java
+++ ch.allon.redskin.ui/src/ch/allon/redskin/internal/ui/views/ProductView.java
@@ -3,8 +3,13 @@ package ch.allon.redskin.internal.ui.views;
 import java.util.Arrays;
 import java.util.Comparator;
 
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.edit.ui.provider.AdapterFactoryContentProvider;
+import org.eclipse.jface.viewers.AbstractTreeViewer;
+import org.eclipse.jface.viewers.DoubleClickEvent;
+import org.eclipse.jface.viewers.IDoubleClickListener;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jface.viewers.StructuredViewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
@@ -15,7 +20,10 @@ import org.eclipse.ui.dialogs.PatternFilter;
 import ch.allon.redskin.core.DBFactory;
 import ch.allon.redskin.core.model.shop.Product;
 import ch.allon.redskin.core.model.shop.ProductCategory;
+import ch.allon.redskin.core.model.shop.ShopPackage;
 import ch.allon.redskin.internal.ui.actions.EObjectAction;
+import ch.allon.redskin.internal.ui.actions.EditProductAction;
+import ch.allon.redskin.internal.ui.actions.EditProductCategoryAction;
 import ch.allon.redskin.internal.ui.actions.NewProductCategoryAction;
 
 public class ProductView extends EObjectView {
@@ -29,6 +37,26 @@ public class ProductView extends EObjectView {
 	protected StructuredViewer createViewer(Composite parent) {
 		FilteredTree tree = new FilteredTree(parent, SWT.MULTI | SWT.H_SCROLL
 				| SWT.V_SCROLL | SWT.BORDER, new PatternFilter(), true);
+		tree.getViewer().setAutoExpandLevel(AbstractTreeViewer.ALL_LEVELS);
+		tree.getViewer().addDoubleClickListener(new IDoubleClickListener() {
+
+			@Override
+			public void doubleClick(DoubleClickEvent event) {
+				EObject obj = (EObject) ((StructuredSelection) event
+						.getSelection()).getFirstElement();
+				EObjectAction a = null;
+				if (obj.eClass().getClassifierID() == ShopPackage.PRODUCT)
+					a = new EditProductAction();
+				else if (obj.eClass().getClassifierID() == ShopPackage.PRODUCT_CATEGORY)
+					a = new EditProductCategoryAction();
+				if (a != null) {
+					a.selectionChanged(null, event.getSelection());
+					a.setActivePart(null, ProductView.this);
+					a.run();
+				}
+
+			}
+		});
 		return tree.getViewer();
 	}
 
