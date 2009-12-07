2608a7b7f56c538f8b0bcd0e4d0d2d966c15108e
allon.moritz <allon.moritz@b4a91378-5cb6-11de-b31f-81d54249e7d9>
2009-12-07 20:36:58 +0000

    more or less it is working

--- trunk/ch.allon.redskin.ui/src/ch/allon/redskin/internal/ui/actions/DeleteTransactionAction.java
+++ trunk/ch.allon.redskin.ui/src/ch/allon/redskin/internal/ui/actions/DeleteTransactionAction.java
@@ -5,22 +5,32 @@ package ch.allon.redskin.internal.ui.actions;
 
 import java.util.List;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.emf.ecore.EObject;
 
 import ch.allon.redskin.core.model.shop.Transaction;
+import ch.allon.redskin.internal.ui.IJobRunnable;
+import ch.allon.redskin.internal.ui.UIUtil;
 
 public class DeleteTransactionAction extends EObjectAction {
 
 	@Override
-	protected void run(List<EObject> selectedObjects) {
-		if (selectedObjects.size() < 1)
-			return;
-		for (EObject obj : selectedObjects) {
-			if (obj instanceof Transaction) {
-				Transaction t = (Transaction) obj;
-				t.getOrder().getTransactions().remove(t);
+	protected void run(final List<EObject> selectedObjects) {
+		UIUtil.runUIJob(new IJobRunnable() {
+
+			@Override
+			public IStatus run(IProgressMonitor monitor) {
+				for (EObject obj : selectedObjects) {
+					if (obj instanceof Transaction) {
+						Transaction t = (Transaction) obj;
+						t.getOrder().getTransactions().remove(t);
+					}
+				}
+				return Status.OK_STATUS;
 			}
-		}
+		});
 	}
 
 }
